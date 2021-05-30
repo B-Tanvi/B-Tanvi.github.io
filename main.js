8fcfddf81f4dae7dc84631792664d0fc3e07e131
Tanvi Bhankhar <67865089+B-Tanvi@users.noreply.github.com>
2021-05-30 17:49:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 var x=document.getElementById("tanvi").width
-console.log(x)
-document.getElementById("icons").style.width=x
-console.log(document.getElementById("icons.style.width"))
+ console.log(x)
+document.getElementById("icons").style.width=x+"px"
+console.log(document.getElementById("icons").style.width)
